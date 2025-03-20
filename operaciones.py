a67e9ce8ab5c6f13b727e3ff160cd72ab25e33f1
Rodrigo Monje <rodrigo.monje.lucas@gmail.com>
2025-03-19 21:34:53 -0400

    Implementacion de Resta

--- operaciones.py
+++ operaciones.py
@@ -4,12 +4,18 @@ def suma(a: int, b: int) -> int:
         return None
     return a + b
 
+def resta(a: int, b: int) -> int:
+    if a is None or b is None:
+        return None
+    return a - b
+
 
 def menu():
     while True:
         print("\nMenú de Operaciones")
         print("1. Sumar dos números")
-        print("2. Salir")
+        print("2. Restar dos números")
+        print("3. Salir")
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
@@ -20,7 +26,17 @@ def menu():
                 print(f"El resultado de la suma es: {resultado}")
             except ValueError:
                 print("Error: Ingrese solo números enteros.")
-        elif opcion == "2":
+
+        if opcion == "2":
+            try:
+                a = int(input("Ingrese el primer número: "))
+                b = int(input("Ingrese el segundo número: "))
+                resultado = resta(a, b)
+                print(f"El resultado de la resta es: {resultado}")
+            except ValueError:
+                print("Error: Se a generado un error, vuelva a intentarlo.")
+
+        elif opcion == "3":
             print("Saliendo del programa...")
             break
         else:
