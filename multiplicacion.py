b26a089fba998c06707c8558d970474a0bdb39ec
Josue Arturo Gonzales Tincuta <202209918@est.umss.edu>
2025-03-19 22:03:32 -0400

    funcion para multiplicar

---
+++ multiplicacion.py
@@ -0,0 +1,13 @@
+#Gonzales Tincuta Josue Arturo
+def multiplicacion(a: float, b: float) -> float:
+    return a * b
+
+if __name__ == "__main__":
+    try:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        
+        resultado = multiplicacion(num1, num2)
+        print(f"La multiplicación de {num1} y {num2} es: {resultado}")
+    except ValueError:
+        print("Error: Por favor, ingrese solo números válidos.")
